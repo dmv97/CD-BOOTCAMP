c6259006da61742425ea10e6932f41bd42fc00ce
dante_mare <59146205+dmv97@users.noreply.github.com>
2024-11-07 14:52:11 -0300

    Update bucle_for_basico1.py

--- bucle_for_basico1.py
+++ bucle_for_basico1.py
@@ -1,14 +1,22 @@
-#BOOTCAMP CODING DOJO - Introducción a Python / Bucles for: Básico 1 (Core)
+# Cabecero informativo
+print("# BOOTCAMP CODING DOJO - Introducción a Python / Bucles for: Básico 1 (Core)")
+print("# Alumno: DANTE MARENGO\n")
+print("="*20 + "\n")
 
-#1 Básico
+# Ejercicio 1: Básico
+print("Ejercicio 1: Básico")
 for i in range(101):
     print(i)
+print("\n" + "="*15 + "\n")  # separador para que sea mas legible
 
-#2 Múltiples de 2
+# Ejercicio 2: Múltiples de 2
+print("Ejercicio 2: Múltiples de 2")
 for i in range(2, 501, 2):
     print(i)
-    
-#3 Contando Vanilla Ice
+print("\n" + "="*15 + "\n")  # separador 
+
+# Ejercicio 3: Contando Vanilla Ice
+print("Ejercicio 3: Contando Vanilla Ice")
 for i in range(1, 101):
     if i % 10 == 0:
         print("baby")
@@ -16,18 +24,24 @@ for i in range(1, 101):
         print("ice ice")
     else:
         print(i)
-        
-#4 Wow. Número gigante a la vista
+print("\n" + "="*15 + "\n")  # separador 
+
+# Ejercicio 4: Wow. Número gigante a la vista
+print("Ejercicio 4: Wow. Número gigante a la vista")
 suma_total = 0
 for i in range(0, 500001, 2):
     suma_total += i
-print(suma_total)
+print("Suma Totla:", suma_total)
+print("\n" + "="*15 + "\n")  # separador 
 
-#5 Regrésame al 3
+# Ejercicio 5: Regrésame al 3
+print("Ejercicio 5: Regrésame al 3")
 for i in range(2024, 0, -3):
     print(i)
-    
-#6 Contador dinámico
+print("\n" + "="*20 + "\n")  # separador
+
+# Ejercicio 6: Contador dinámico
+print("Ejercicio 6: Contador dinámico")
 numInicial = 3
 numFinal = 10
 multiplo = 2
@@ -35,7 +49,11 @@ multiplo = 2
 for i in range(numInicial, numFinal + 1):
     if i % multiplo == 0:
         print(i)
+print("\n" + "="*20 + "\n")  # separador Final 
 
-#   (\(\
-#   ( -.-)
-#    o_(")(")
+
+print(r"""
+   (\(\
+   ( -.-)
+    o_(")(")
+""")
