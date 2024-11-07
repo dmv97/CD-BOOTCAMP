e71c042193d0c4fbde8352e0d92af01b223f94ba
dante_mare <59146205+dmv97@users.noreply.github.com>
2024-11-07 14:36:48 -0300

    Update bucle_for_basico1.py

--- bucle_for_basico1.py
+++ bucle_for_basico1.py
@@ -1,14 +1,14 @@
 #BOOTCAMP CODING DOJO - Introducción a Python / Bucles for: Básico 1 (Core)
 
-#1 print del 0 al 100
+#1 Básico
 for i in range(101):
     print(i)
 
-#2 multiplos de 2
+#2 Múltiples de 2
 for i in range(2, 501, 2):
     print(i)
     
-#3 vanilla ice
+#3 Contando Vanilla Ice
 for i in range(1, 101):
     if i % 10 == 0:
         print("baby")
@@ -17,17 +17,17 @@ for i in range(1, 101):
     else:
         print(i)
         
-#4 numero gigante
+#4 Wow. Número gigante a la vista
 suma_total = 0
 for i in range(0, 500001, 2):
     suma_total += i
 print(suma_total)
 
-#5 volver al 3
+#5 Regrésame al 3
 for i in range(2024, 0, -3):
     print(i)
     
-#6 contar dinamicamente
+#6 Contador dinámico
 numInicial = 3
 numFinal = 10
 multiplo = 2
@@ -35,7 +35,7 @@ multiplo = 2
 for i in range(numInicial, numFinal + 1):
     if i % multiplo == 0:
         print(i)
-        
+
 #   (\(\
 #   ( -.-)
 #    o_(")(")
